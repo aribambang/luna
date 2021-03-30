5b8dfd4742494532833fb10547e02b8825ec08f4
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-03-30 16:41:19 +0700

    add API for logout

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -48,4 +48,8 @@ class AuthController extends Controller
   public function user(Request $request){
     return new UserResource($request->user());
   }
+
+  public function logout(){
+    auth()->logout();
+  }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'Register']);
 Route::post('/login', [AuthController::class, 'Login']);
 Route::get('/user', [AuthController::class, 'User']);
+Route::get('/logout', [AuthController::class, 'Logout']);
